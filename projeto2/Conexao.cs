bf333c5338ea159748319d7689135181d63006bf
Wudson Melo <wudson.melo@fatec.sp.gov.br>
2019-10-13 23:19:04 -0300

    adição da classe de conexão

---
+++ projeto2/Conexao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace projeto2
+{
+    public static class Conexao
+    {
+        private static FbConnection InstanciaFireBird { get; set; }
+
+        private const string StringConexao =
+            "User = SYSDBA; Password=masterkey;Database=C:/Users/Wudso/Documents/IBExpert_Portable/banco.fdb;DataSource=localhost; Pooling=true;";
+
+
+        public static FbConnection GetInstancia() => InstanciaFireBird ?? CriaConexao();
+
+        public static FbConnection CriaConexao() => InstanciaFireBird = new FbConnection(StringConexao);
+    }
+}
